7a0e5a396ca98da00b154375dab636241c637e26
searleser97 <gabrielsanv97@gmail.com>
2023-08-25 17:35:57 -0400

    add harpoon mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -14,9 +14,19 @@ vim.keymap.set('i', '<cr>', '<space><c-g>u', { noremap = true })
 -- end fine-grained undo
 
 if not vim.g.vscode then
-  local builtin = require('telescope.builtin')
-  vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-  vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-  vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-  vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+  local telescope_builtin = require('telescope.builtin')
+  vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, {})
+  vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep, {})
+  vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, {})
+  vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, {})
+
+  local harpoon_ui = require("harpoon.ui")
+  vim.keymap.set('n', '<leader>ha', require("harpoon.mark").add_file, {})
+  vim.keymap.set('n', '<leader>ht', harpoon_ui.toggle_quick_menu, {})
+  vim.keymap.set('n', '<leader>hn', harpoon_ui.nav_next, {})
+  vim.keymap.set('n', '<leader>hp', harpoon_ui.nav_prev, {})
+  vim.keymap.set('n', '<leader>1', function() harpoon_ui.nav_file(1) end, {})
+  vim.keymap.set('n', '<leader>2', function() harpoon_ui.nav_file(2) end, {})
+  vim.keymap.set('n', '<leader>3', function() harpoon_ui.nav_file(3) end, {})
+  vim.keymap.set('n', '<leader>4', function() harpoon_ui.nav_file(4) end, {})
 end
