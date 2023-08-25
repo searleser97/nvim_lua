fc11b281cf6eb83e4f4082eb50538e689562440c
searleser97 <gabrielsanv97@gmail.com>
2023-08-24 21:18:21 -0400

    update

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,3 +7,7 @@ vim.keymap.set({'n', 'x'}, '<C-b>', '<C-v>', { noremap = true })
 vim.keymap.set({'x', 'n'}, 'p', '"0p', { noremap = true })
 vim.keymap.set({'n', 'x', 'o'}, '[', '""p', { noremap = true })
 vim.keymap.set('n', '<cr>', 'i<cr><esc>', { noremap = true })
+-- fine-grained undo
+vim.keymap.set('i', '<space>', '<space><c-g>u', { noremap = true })
+vim.keymap.set('i', '<tab>', '<space><c-g>u', { noremap = true })
+vim.keymap.set('i', '<cr>', '<space><c-g>u', { noremap = true })
