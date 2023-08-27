62cd2b15081eb4e0a05e2a3033dbd94d24fc5810
Sergio <gabrielsanv97@gmail.com>
2023-08-26 22:43:43 -0700

    Update mappings.lua

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,7 +1,11 @@
 vim.g.mapleader = " "
 vim.cmd("unmap [%")
 
-vim.keymap.set({'n', 'x'}, '<C-c>', '"+y', { noremap = true })
+if package.config:sub(1,1) == "\\" then
+  vim.keymap.set({'n', 'x'}, '<C-c>', '"*y', { noremap = true })
+else
+  vim.keymap.set({'n', 'x'}, '<C-c>', '"+y', { noremap = true })
+end
 vim.keymap.set({'n', 'x'}, '<C-v>', '"+p', { noremap = true })
 vim.keymap.set({'n', 'x'}, '<C-b>', '<C-v>', { noremap = true })
 vim.keymap.set({'x', 'n'}, 'p', '"0p', { noremap = true })
