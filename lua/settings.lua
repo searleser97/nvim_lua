64d904c91b34d6bf07e463ef4c024ed0f8a79297
searleser97 <gabrielsanv97@gmail.com>
2023-08-31 09:57:29 -0700

    update

--- lua/settings.lua
+++ lua/settings.lua
@@ -39,3 +39,5 @@ if not vim.g.vscode then
   -- vim.cmd("colorscheme catppuccin-mocha")
   vim.cmd("colorscheme tokyonight-night")
 end
+
+vim.cmd("autocmd CursorHold * lua vim.diagnostic.open_float()")
