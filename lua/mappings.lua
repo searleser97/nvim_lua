cbb099e3f38170ccaf8edfbd0d7d7324e8a1b43f
Sergio <gabrielsanv97@gmail.com>
2023-09-05 14:10:46 -0700

    Update mappings.lua

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,7 +8,7 @@ else
 end
 vim.keymap.set({'n', 'x'}, '<C-v>', '"+p', { noremap = true })
 vim.keymap.set({'n', 'x'}, '<C-b>', '<C-v>', { noremap = true })
-vim.keymap.set({'x', 'n'}, 'p', '"0p', { noremap = true })
+vim.keymap.set({'x', 'n'}, 'l', '"0p', { noremap = true })
 vim.keymap.set({'n', 'x', 'o'}, '[', '""p', { noremap = true })
 vim.keymap.set('n', '<cr>', 'i<cr><esc>', { noremap = true })
 vim.keymap.set('x', 'y', "ygv<esc>", { noremap = true })
