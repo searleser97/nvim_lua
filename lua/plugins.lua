1033aba70cb590f09f331abaac4c143cd2019a58
searleser97 <gabrielsanv97@gmail.com>
2023-09-26 08:57:15 -0700

    remove neogit

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -251,23 +251,6 @@ require("lazy").setup({
     end
   },
   {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim",         -- required
-      "nvim-telescope/telescope.nvim", -- optional
-      "sindrets/diffview.nvim",        -- optional
-    },
-    config = function ()
-      require("neogit").setup({
-        integrations = {
-          telescope = true,
-          diffview = true,
-        }
-      })
-    end,
-    cond = not vim.g.vscode
-  },
-  {
     "nvim-tree/nvim-web-devicons",
     cond = not vim.g.vscode
   },
