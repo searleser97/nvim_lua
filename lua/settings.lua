b5580298a508e748e7e0c1f40efc272f110a1a66
searleser97 <gabrielsanv97@gmail.com>
2023-10-24 14:22:03 -0700

    update

--- lua/settings.lua
+++ lua/settings.lua
@@ -5,6 +5,7 @@ vim.opt.relativenumber = true
 vim.wo.number = true
 -- vim.opt.tabstop = 2
 -- vim.opt.softtabstop = 2
+vim.opt.smartindent = true
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.wrap = false
