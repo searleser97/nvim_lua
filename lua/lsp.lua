8a3bb95edafaa1d61488e17a3363c3f0ae1446f4
searleser97 <gabrielsanv97@gmail.com>
2024-10-18 11:35:01 -0700

    jump to diagnostic window when opening it

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -5,9 +5,15 @@ if not vim.g.vscode then
     lsp_zero.default_keymaps({ buffer = buffer })
     local telescope_builtin = require('telescope.builtin')
     vim.keymap.set('n', 'gd', telescope_builtin.lsp_definitions, { noremap = true, desc = "go to definition" })
+    vim.keymap.set('n', 'gd', 'gdzz', { desc = "go to definition" })
     vim.keymap.set('n', 'gi', telescope_builtin.lsp_implementations, { noremap = true, desc = "go to implementation" })
+    vim.keymap.set('n', 'gi', 'gizz', { desc = "go to implementation" })
     vim.keymap.set('n', 'gr', telescope_builtin.lsp_references, { noremap = true, desc = "go to references" })
-    vim.keymap.set('n', '<leader>sd', vim.diagnostic.open_float, { noremap = true, desc = "show diagnostics" })
+    vim.keymap.set('n', 'gr', 'grzz', { desc = "go to references" })
+    vim.keymap.set('n', '<leader>sd', function()
+      vim.diagnostic.open_float()
+      vim.diagnostic.open_float() -- the second call moves my cursor inside the diagnostic window
+    end, { noremap = true, desc = "show diagnostics" })
   end)
 
   require("mason").setup({})
