8f14a0a5dde3e663949aeb447b5c018507e2bae1
searleser97 <gabrielsanv97@gmail.com>
2024-10-21 13:48:21 -0700

    remove rust projects

--- lua/rust_lua_ffi_example/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod extern_ffi {
-    pub fn hello_world() -> String {
-        "Hello World!".to_owned()
-    }
-}
-
-include!(concat!(env!("OUT_DIR"), "/ffi.rs"));
--- nvim_utils/src/build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-  println!("cargo:rustc-link-search=E:\\forks\\luajit\\src");
-  println!("cargo:rustc-link-lib=lua51");
-  println!("cargo:rustc-link-lib=luajit");
-}
