8526f1822fa148e2427b1782bbdac6f6cb2ba69a
searleser97 <gabrielsanv97@gmail.com>
2024-10-21 13:31:04 -0700

    update lsp bindings

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -2,17 +2,22 @@ if not vim.g.vscode then
   local lsp_zero = require("lsp-zero").preset({})
 
   lsp_zero.on_attach(function(client, buffer)
-    lsp_zero.default_keymaps({ buffer = buffer })
+    -- lsp_zero.default_keymaps({ buffer = buffer })
     lsp_zero.highlight_symbol(client, buffer)
     local telescope_builtin = require('telescope.builtin')
     vim.keymap.set('n', 'gd', telescope_builtin.lsp_definitions, { noremap = true, desc = "go to definition" })
     vim.keymap.set('n', 'gi', telescope_builtin.lsp_implementations, { noremap = true, desc = "go to implementation" })
     vim.keymap.set('n', 'gr', telescope_builtin.lsp_references, { noremap = true, desc = "go to references" })
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { noremap = true, desc = "go to references" })
     vim.keymap.set('n', '<leader>sd', function()
       vim.diagnostic.open_float()
       vim.diagnostic.open_float() -- the second call moves my cursor inside the diagnostic window
     end, { noremap = true, desc = "show diagnostics" })
-  end)
+    vim.keymap.set('n', '<leader>si', function()
+      vim.lsp.buf.hover()
+      vim.lsp.buf.hover()
+    end, { noremap = true, desc = "show symbol info" })
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { noremap = true, desc = "code action" }) end)
 
   require("mason").setup({})
   require("mason-lspconfig").setup({
