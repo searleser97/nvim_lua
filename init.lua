8b3105762a5bf560698645875d93f36dc5405893
searleser97 <gabrielsanv97@gmail.com>
2024-10-25 13:43:04 -0700

    improve cursormoved autocmd

--- init.lua
+++ init.lua
@@ -8,13 +8,16 @@ vim.api.nvim_create_autocmd({ 'CursorMoved' }, {
   callback = (function()
     local initialCursorPos = vim.fn.getcurpos()
     local prevLine = initialCursorPos[2]
+    local prevWindow = vim.api.nvim_get_current_win()
     return function()
       local curr_cursor_pos = vim.fn.getcurpos()
       local currLine = curr_cursor_pos[2]
-      if (math.abs(prevLine - currLine) > 10) then
+      local currWindow = vim.api.nvim_get_current_win()
+      if math.abs(prevLine - currLine) > 10 and prevWindow == currWindow then
         vim.cmd("norm! zz")
       end
       prevLine = currLine
+      prevWindow = currWindow
     end
   end)()
 })
