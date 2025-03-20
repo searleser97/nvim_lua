a4aa89e3de3046f5ecd48066245f661d59537d69
searleser97 <gabrielsanv97@gmail.com>
2025-03-20 11:15:48 -0700

    update conform

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1254,6 +1254,9 @@ require("lazy").setup({
       formatters_by_ft = {
         lua = { "stylua" },
         javascript = { "prettierd", "prettier", stop_after_first = true },
+        typescript = { "prettierd", "prettier", stop_after_first = true },
+        typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+        javascriptreact = { "prettierd", "prettier", stop_after_first = true },
         rust = { "rustfmt", lsp_format = "fallback" },
       },
       -- Set default options
